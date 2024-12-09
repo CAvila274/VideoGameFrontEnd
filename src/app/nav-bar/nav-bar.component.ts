7beefafdb3c4904ed6c768b3b3bdd50b92c536f7
CAvila274 <47956112+CAvila274@users.noreply.github.com>
2024-12-09 09:59:26 -0800

    12/9 update

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import {Router, RouterLink, RouterOutlet } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbar} from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { Subject, takeUntil } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
@@ -17,9 +19,19 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
-  onLogOut() {
-    throw new Error('Method not implemented.');
-    }
-      loggedIn: boolean = false;
 
+private destroySubject = new Subject();
+loggedIn: boolean = false;
+
+constructor(private authService: AuthService, private router: Router){
+  authService.authStatus.pipe(takeUntil(this.destroySubject))
+  .subscribe(result => {
+    this.loggedIn = result;
+  })
+}
+  
+  onLogOut() {
+   this.authService.logout();
+   this.router.navigate(["/login"]);
+  }
 }
